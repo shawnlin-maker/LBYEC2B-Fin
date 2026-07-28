6fe68a334521552d7705e66677185b8a52e7870c
shawnlin-maker <sean_edric_lee_a@dlsu.edu.ph>
2026-07-28 13:28:24 +0800

    added cannon and sky

--- cannon_sim.m
+++ cannon_sim.m
@@ -3,6 +3,8 @@ function cannon_sim
     %% plotting window/canvas
     fig = figure;
     ax = axes('Parent', fig);
+    set(ax, 'Color', [0.85 0.92 1]);
+    set(fig, 'Color', [1 1 1])
     axis(ax, [0 200 0 100]);
     hold(ax, 'on');
 
@@ -10,13 +12,14 @@ function cannon_sim
     ballPlot = plot(ax, 0, 0, 'o', 'MarkerSize', 14, 'MarkerFaceColor', "r");
 
     %% create tracers
-    tracerPlot = animatedline(ax, 'color', 'r', 'linewidth', 2)
+    tracerPlot = animatedline(ax, 'color', 'r', 'linewidth', 2);
 
     %% create cannon and variables
     pivot = [3,4];
     cannonLen = 8;
 
-    cannonPlot = plot(ax, [pivot(1) pivot(1) + cannonLen * cosd(45)], [pivot(2) pivot(2) + cannonLen * sind(45)], 'k', 'linewidth', 4)
+    cannonPlot = plot(ax, [pivot(1) pivot(1) + cannonLen * cosd(45)], [pivot(2) pivot(2) + cannonLen * sind(45)], 'k', 'Color', [0.2 0.2 0.2], 'LineWidth', 8);
+    rectangle(ax, 'Position', [pivot(1)-2 0 4 pivot(2)], 'Curvature', 0.3, 'FaceColor', [0.3 0.3 0.3]);
 
     %% velocity values
     vxText = text(ax, [150], [95], "Vx: 0m/s");
@@ -26,13 +29,15 @@ function cannon_sim
     frameCount = 0;
 
     %% sliders/menu
-    vSlider = uicontrol('Parent', fig, 'Style', 'slider', 'Min', 0, 'Max', 200, 'Value', 40, 'Position', [50 40 150 20]);
+    vSlider = uicontrol('Parent', fig, 'Style', 'slider', 'Min', 0, 'Max', 60, 'Value', 30, 'Position', [50 40 150 20]);
 
     angleSlider = uicontrol('Parent', fig, 'Style', 'slider', 'Min', 0, 'Max', 90, 'Value', 45, 'Position', [250 40 150 20], 'Callback', @(src,evt) cannonAngle(src, cannonPlot, cannonLen, pivot));
 
     uicontrol('Style', 'pushbutton', 'String', 'Fire!', 'Position', [450 40 100 30], 'Callback', @(src,evt) fireCannon(ax, ballPlot, vSlider, angleSlider));
 
     matMenu = uicontrol('Style', 'popupmenu', 'String', {'Steel', 'Rubber', 'Wood'}, 'Position', [600 40 100 20]);
+
+    spaceMenu = uicontrol('Style', 'pushbutton', 'String', {'Vacuum', 'Earth (realistic)'}, 'Position', [600 70 100 20])
 
 
     %% cannon barrel angle
@@ -67,7 +72,7 @@ function cannon_sim
             while y >= 0
                 
                 
-                [x, y, vx, vy] = step_physics(x, y, vx, vy, dt, g)
+                [x, y, vx, vy] = step_physics(x, y, vx, vy, dt, g, useDrag)
                 set(ballPlot, 'XData', x, 'YData', y);
                 
                 addpoints(tracerPlot, x, y);
@@ -75,7 +80,6 @@ function cannon_sim
                 frameCount = frameCount + 1 
 
                 live_display(vx,vy, vxText, vyText)
-
 
                 if mod(frameCount, 50) == 0
                     quiver(ax,x,y,vx*0.5,vy*0.5, 'color',[0 0.4 1],'MaxHeadSize', 1);
--- step_physics.m
+++ step_physics.m
@@ -1,7 +1,8 @@
 
 
-function [x, y, vx, vy] = step_physics(x, y, vx, vy, dt, g)
-vy = vy - g*dt;
-x = x + vx*dt; %% euler approximation, not real kinematics equation
-y = y + vy*dt; %% euler approximation, not real kinematics equation
+function [x, y, vx, vy] = step_physics(x, y, vx, vy, dt, g, useDrag)
+
+    vy = vy - g*dt;
+    x = x + vx*dt; %% euler approximation, not real kinematics equation
+    y = y + vy*dt; %% euler approximation, not real kinematics equation
 end
