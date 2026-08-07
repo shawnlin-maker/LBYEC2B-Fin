5bfb7cf02cde9c2e6825a99e208bc704b770d9c1
shawnlin-maker <sean_edric_lee_a@dlsu.edu.ph>
2026-08-07 19:35:07 +0800

    m

--- step_physics.m
+++ step_physics.m
@@ -1,13 +1,15 @@
 
 
-function [x, y, vx, vy] = step_physics(x, y, vx, vy, dt, g, space, Cd, mass)
+function [x, y, vx, vy, bounced] = step_physics(x, y, vx, vy, dt, g, space, Cd, mass, restitution)
     speed = sqrt(vx^2 + vy^2);
 
     if space == 1
+        %% vacuum no drag
         ax = 0;
         ay = -g;
     
     elseif space == 2
+        %% realistic with drag
         rho = 1.225;
         A = 0.05;
 
@@ -23,4 +25,11 @@ function [x, y, vx, vy] = step_physics(x, y, vx, vy, dt, g, space, Cd, mass)
     vy = vy + ay*dt;
     x = x + vx*dt; %% euler approximation, not real kinematics equation
     y = y + vy*dt; %% euler approximation, not real kinematics equation
+
+    %% ground bounce -- flip and shrink vertical velocity based on material bounciness
+    if y <= 0
+        y = 0;
+        vy = -vy * restitution;
+        bounced = true;
+    end
 end
